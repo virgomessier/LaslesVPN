0d82a6ee68da7535a0206ad9e059965a727a6a8e
virgomessier104 <virgomessier104@protonmail.com>
2024-01-12 01:19:53 +0400

    V2.2 made 80% css, added swiper

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,50 @@
 /*eslint-env es6, es2017, es2018*/
 /*eslint-disable no-console,*/
 /*global console*/
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+	},
+	
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	
+	// And if we need scrollbar
+	// scrollbar: {
+	// 	el: '.swiper-scrollbar',
+	// },
+
+	slidesPerView: 2.5,
+
+	speed: 700,
+	spaceBetween: 50,
+
+	// autoplay: {
+	// 	delay: 3000,
+	// },
+
+	// effect: 'coverflow',
+
+	// CoverflowEffect: {
+	// 	rotate: 20,
+	// 	stretch: 50,
+	// },
+
+	keyboard: {
+		enable: true,
+		onlyInViewport: true,
+		PageUpDown: false,
+	},
+
+});
